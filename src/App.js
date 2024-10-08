713bdc9cbc9b9c94d22e87b9cfa0c04a33a7002c
halu1031 <lukas.haeffele@icloud.com>
2024-10-08 15:53:09 +0200

    First Lottie Animation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import Lottie from "react-lottie";
+import animationData from './lotties/Animation - 1728395459462np';
+
+
 function App() {
-  return (
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+
+
+  return (<>
     <h1>Multimedia Projekt</h1>
+
+      <div>
+       <Lottie 
+	        options={defaultOptions}
+          height={400}
+          width={400}
+        />
+      </div>
+    </>
   );
 }
 
